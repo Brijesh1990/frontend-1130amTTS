a3ec5bb4d4a833e26143324d69a48cbe1d1a3aab
Brijesh1990 <bkpandey.pandey@gmail.com>
2023-07-22 12:15:48 +0530

    Add files via upload

---
+++ module6/javascript/backup-22-07-2023/js-functions/js/address.js
@@ -0,0 +1,22 @@
+function address1()
+{
+   
+    var add="Jalaram Icecream rajkot <br>150 feet ring road near <br> telephone exchange b/h sbi bank  Rajkot-36005";
+    document.write(add);
+    document.getElementById("add1").style="color:blue; font-size:20px";
+}
+function address2()
+{
+    var add="&#9786 Jalaram Icecream rajkot <br>50 feet ring road near <br> Near alap green city b/h sbi bank  Rajkot-36005";
+    document.write(add);
+}
+function address3()
+{
+    var add="Jalaram Icecream rajkot <br>Gondal chowk  150 feet ring road near <br> telephone exchange b/h sbi bank  Rajkot-36005";
+    document.write(add);
+}
+function address4()
+{
+    var add="Jalaram Icecream rajkot <br>behind jalaram plot-2 indira circle <br> telephone exchange b/h sbi bank  Rajkot-36005";
+    document.write(add);
+}
